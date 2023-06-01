4ed6cdab8ca47dc23f30435f769113d4be2fc355
Diellza Raçi <115530290+didiellzaza@users.noreply.github.com>
2023-06-01 10:58:03 +0200

    Update and rename client2.c to client8.c

--- client8.c
+++ client8.c
@@ -4,6 +4,7 @@
 #include <sys/ipc.h>
 #include <sys/msg.h>
 #include <string.h>
+#include <sys/types.h>
 
 #define SERVER_KEY 1234
 #define CLIENT_KEY 5678
@@ -11,18 +12,17 @@
 typedef struct {
     long mtype;
     char data[256];
+    pid_t pid;
 } Message;
 
 int main() {
     int server_queue_id, client_queue_id;
 
-    // Create client message queue
     if ((client_queue_id = msgget(CLIENT_KEY, IPC_CREAT | 0666)) == -1) {
         perror("msgget");
         exit(1);
     }
 
-    // Connect to the server
     if ((server_queue_id = msgget(SERVER_KEY, 0)) == -1) {
         perror("msgget");
         exit(1);
@@ -30,50 +30,58 @@ int main() {
 
     Message connect_msg;
     connect_msg.mtype = 1;
-    connect_msg.data[0] = '\0'; // No data for connection request
+    connect_msg.data[0] = '\0';
+    connect_msg.pid = getpid();
 
     if (msgsnd(server_queue_id, &connect_msg, sizeof(Message) - sizeof(long), 0) == -1) {
         perror("msgsnd");
         exit(1);
     }
 
-    printf("Konektuar ne server\n");
+    printf("Konektuar me server\n");
 
     while (1) {
-        // Send a request to the server
         Message request;
         request.mtype = CLIENT_KEY;
-
-        printf("Shtyp kerkesen: ");
+        request.pid = getpid();
+        printf("Shkruaj kerkesen/mesazhin: ");
         fgets(request.data, sizeof(request.data), stdin);
 
-        // Remove newline character from the input
         size_t len = strlen(request.data);
         if (len > 0 && request.data[len - 1] == '\n') {
             request.data[len - 1] = '\0';
         }
 
-        // Send the request to the server
         if (msgsnd(client_queue_id, &request, sizeof(Message) - sizeof(long), 0) == -1) {
             perror("msgsnd");
             exit(1);
         }
+
+        Message response;
+        if (msgrcv(client_queue_id, &response, sizeof(Message) - sizeof(long), 0, 0) == -1) {
+            perror("msgrcv");
+            exit(1);
+        }
+
+        printf("Pergjigja nga serveri: %s\n", response.data);
+
+        if (strcmp(request.data, "disconnect") == 0) {
+            printf("Diskonektuar nga serveri\n");
+            break;
+        }
     }
 
-    // Disconnect from the server
     Message disconnect_msg;
     disconnect_msg.mtype = 1;
-    disconnect_msg.data[0] = '\0'; // No data for disconnect request
+    strcpy(disconnect_msg.data, "disconnect");
+    disconnect_msg.pid = getpid();
 
     if (msgsnd(server_queue_id, &disconnect_msg, sizeof(Message) - sizeof(long), 0) == -1) {
         perror("msgsnd");
+        exit(1);
     }
 
-    // Clean up client message queue
     msgctl(client_queue_id, IPC_RMID, NULL);
-
-    printf("Ckycur nga serveri\n");
 
     return 0;
 }
-
